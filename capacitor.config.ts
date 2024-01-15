c6ab416845345480c6cda553d64f22602aa6a297
unknown <Cetecom@SJAPCA03LC0627.duoc.acad>
2024-01-15 09:22:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.aplicacion006',
+  appName: 'aplicacion006',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
